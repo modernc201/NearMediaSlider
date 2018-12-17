2cec3698e52e8f1f9d2afbaacf6d832d3483defa
Lyo <modernly201@gmail.com>
2018-12-17 18:45:33 +0900

    Change the Image Library Add Glide

--- app/src/main/java/com/moderncreation/mediasliderlibrary/SimpleImageFragment.java
+++ app/src/main/java/com/moderncreation/mediasliderlibrary/SimpleImageFragment.java
@@ -4,15 +4,15 @@ package com.moderncreation.mediasliderlibrary;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.moderncreation.mediasliderlibrary.View.MediaSliderItem;
 import com.moderncreation.mediasliderlibrary.databinding.FragmentSimpleImageBinding;
-import com.squareup.picasso.Picasso;
 
 
 /**
@@ -60,11 +60,10 @@ import com.squareup.picasso.Picasso;
         FragmentSimpleImageBinding binding = FragmentSimpleImageBinding.inflate(inflater, container, false);
         imageView = binding.sliderImg;
 
-        Picasso.with(getContext())
+
+        Glide.with(getContext())
                 .load(sURL)
-                .fit()
-                .centerInside()
-                .placeholder(getPlaceholderImage())
+                .apply(RequestOptions.fitCenterTransform().fitCenter())
                 .into(imageView);
 
         return binding.getRoot();
